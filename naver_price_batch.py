fb37788650248d6fc6440e4a51c909e5099761c6
Sangmoo <first_1st@naver.com>
2025-03-26 00:09:40 +0900

    Fix md

--- naver_price_batch.py
+++ naver_price_batch.py
@@ -3,6 +3,7 @@ import subprocess
 
 app = Flask(__name__)
 
+
 # 🔹 실시간 로그 스트리밍 함수
 def generate_logs():
     process = subprocess.Popen(
@@ -10,7 +11,7 @@ def generate_logs():
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
-        encoding="utf-8"  # 🔹 UTF-8 인코딩 적용
+        encoding="utf-8",  # 🔹 UTF-8 인코딩 적용
     )
 
     # 🔹 실행되는 동안 실시간 로그 반환
@@ -21,10 +22,15 @@ def generate_logs():
     for err in process.stderr:
         yield "[ERROR] " + err.strip() + "\n"
 
-# 🔹 API 호출 시 실시간 로그 반환
-@app.route('/run_batch', methods=['GET'])
-def run_batch():
-    return Response(stream_with_context(generate_logs()), content_type='text/plain; charset=utf-8')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# 🔹 API 호출 시 실시간 로그 반환
+# 🔹 127.0.0.1:5000/run_batch 에 접속하면 배치 서비스 실행
+@app.route("/run_batch", methods=["GET"])
+def run_batch():
+    return Response(
+        stream_with_context(generate_logs()), content_type="text/plain; charset=utf-8"
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Flask 서버 실행 / 127.0.0.1:5000 접속
--- readme.md
+++ readme.md
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 
 # 실행 파일 생성
 pyinstaller Naver_Price.py --onefile    
+
+# Flask 설치
+pip install Flask
+
+# Flask를 이용한 배치 서비스 구현
+python naver_price_backend.py
+127.0.0.1:5000/run_batch 에 접속하면 배치 서비스 실행
+브라우저 화면에 Naver_Price.py 로그를 스트림하게 표시
